8b9949907940dbea2ff2a946f0cac5fe2889ae7b
abdulaziz998kw <abdulaziz998kw@gmail.com>
2026-04-23 22:40:00 +0100

    Add count_clipping and compliance checking functions

--- waveform.c
+++ waveform.c
@@ -19,3 +19,15 @@ double compute_peak_to_peak(double *values, int n) {
 
     return max - min;
 }
+int count_clipping(double *values, int n, double limit) {
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (fabs(values[i]) >= limit) {
+            count++;
+        }
+    }
+    return count;
+}
+int check_compliance(double rms) {
+    return (rms >= 207 && rms <= 253);
+}
--- waveform.h
+++ waveform.h
@@ -14,4 +14,6 @@ typedef struct {
 
 double compute_rms(double *values, int n);
 double compute_peak_to_peak(double *values, int n);
+int count_clipping(double *values, int n, double limit);
+int check_compliance(double rms);
 #endif //POWER_ANALYSER_WAVEFORM_H
