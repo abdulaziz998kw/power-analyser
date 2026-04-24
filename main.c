03e26f191d774a92f0766889ad36e349849dda77
abdulaziz998kw <abdulaziz998kw@gmail.com>
2026-04-24 20:55:00 +0100

    Update main to test load_csv and print number of lines read

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+#include "io.h"
+
 int main(void) {
-    printf("Hello, World!\n");
+
+    int n=0;
+    WaveformSample *data=load_csv("power_quality_log.csv",&n);
+    printf("data read number : %d\n",n);
     return 0;
 }
