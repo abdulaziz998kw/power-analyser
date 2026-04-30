c00db710ef324752c69ea363f19cecf5b50aa339
abdulaziz998kw <abdulaziz998kw@gmail.com>
2026-04-30 20:35:00 +0100

    Update main to accept input file via argc/argv and retest row counting

--- main.c
+++ main.c
@@ -2,10 +2,15 @@
 
 #include "io.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <csv_file>\n", argv[0]);
+        return 1;
+    }
 
     int n=0;
-    WaveformSample *data=load_csv("power_quality_log.csv",&n);
+    WaveformSample *data = load_csv(argv[1], &n);
     printf("data read number : %d\n",n);
     return 0;
 }
